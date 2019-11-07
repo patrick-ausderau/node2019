a495ffa88251d70765d4f800a32d8838d3995ebb
Patrick Ausderau <patrick.ausderau@metropolia.fi>
2019-11-07 13:22:53 +0200

    express app, get / and get /demo

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('my app...');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello form my Node server');
+});
+
+app.get('/demo', (req, res) => {
+  console.log('request', req);
+  res.send('demo');
+});
+
+app.listen(3000, () => {
+  console.log('server app start?');
+});
