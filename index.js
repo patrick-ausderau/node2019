de40d491777a36bd33520c970e06a161ef2fa3f1
Patrick Ausderau <patrick.ausderau@metropolia.fi>
2019-11-15 14:31:32 +0200

    databse connection in separated module exports

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 'use strict';
 
-require('dotenv').config();
-
 const express = require('express');
-const mysql = require('mysql2');
-
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-});
+const connection = require('./model/db.js');
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/animal', (req, res) => {
-  connection.query(
-    'SELECT * FROM animal',
-    (err, results, fields) => {
-      console.log(results); // results contains rows r
-      console.log(fields); // fields contains extra m
-      res.json(results);
-    }
-  );
+app.get('/animal', async (req, res) => {
+  try {
+    const [results, fields] = await connection.query(
+      'SELECT * FROM animal');
+    console.log(results); // results contains rows r
+    console.log(fields); // fields contains extra m
+    res.json(results);
+  } catch (e) {
+    console.log(e);
+    res.send('db error :(');
+  }
 });
 
 app.get('/', (req, res) => {
