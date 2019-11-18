efccd4f61ff198997405b38bf8313a3f35f26b20
Patrick Ausderau <patrick.ausderau@metropolia.fi>
2019-11-18 14:14:06 +0200

    body parser with HTTP POST and mysql INSERT

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const connection = require('./model/db.js');
 
 const app = express();
-
+const bodyParser = require('body-parser');
 app.use(express.static('public'));
 
 app.get('/animals', async (req, res) => {
@@ -22,19 +22,28 @@ app.get('/animals', async (req, res) => {
 
 app.get('/animal', async (req, res) => {
   console.log(req.query);
-  //res.send(`qurey params? ${req.query}`);
   try {
     const [results] = await connection.query(
       'SELECT * FROM animal WHERE name LIKE ?',
       [req.query.name]);
-    /*Never do that... not SQL injection safe!
-      const [results] = await connection.query(
-      `SELECT * FROM animal WHERE name LIKE '${req.query.name}'`);*/
     res.json(results);
   } catch(e) {
     res.send(`db error ${e}`);
   }
-    
+});
+
+app.post('/animal', bodyParser.urlencoded(), async (req, res) => {
+  console.log(req.body);
+  try {
+    const [result] = await connection.query(
+      'INSERT INTO animal  (name) VALUES (?)',
+      [req.body.name]
+    );
+    res.json(result);
+  } catch (e) {
+    console.log(e);
+    res.send('db error');
+  }
 });
 
 app.get('/', (req, res) => {
