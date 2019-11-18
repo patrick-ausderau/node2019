8787c05c4ff4b6b033477b2911f17d55cc6b59c2
Patrick Ausderau <patrick.ausderau@metropolia.fi>
2019-11-18 12:53:57 +0200

    perpare statement for SQL and HTTP GET URL query parameters

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/animal', async (req, res) => {
+app.get('/animals', async (req, res) => {
   try {
     const [results, fields] = await connection.query(
       'SELECT * FROM animal');
@@ -18,6 +18,23 @@ app.get('/animal', async (req, res) => {
     console.log(e);
     res.send('db error :(');
   }
+});
+
+app.get('/animal', async (req, res) => {
+  console.log(req.query);
+  //res.send(`qurey params? ${req.query}`);
+  try {
+    const [results] = await connection.query(
+      'SELECT * FROM animal WHERE name LIKE ?',
+      [req.query.name]);
+    /*Never do that... not SQL injection safe!
+      const [results] = await connection.query(
+      `SELECT * FROM animal WHERE name LIKE '${req.query.name}'`);*/
+    res.json(results);
+  } catch(e) {
+    res.send(`db error ${e}`);
+  }
+    
 });
 
 app.get('/', (req, res) => {
